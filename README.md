f5bd5891d5784578dce8951f9cf653ab27d7fd47
Dan Lee <dan@dataform.co>
2020-04-01 17:03:01 +0000

    create an example package

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dataform_package_base
+
+Update this file to describe your package and how to use it.
+
+An example is [here](https://github.com/dataform-co/dataform-segment/blob/master/README.md).
---
+++ definitions/example.js
@@ -0,0 +1,11 @@
+// Add an example to show how your package should be used
+// It's a good idea to connect your package repository
+// to a warehouse and ensure the package compiles, runs 
+// and creates the expected results
+
+const examplePackage = require("../");
+
+examplePackage({
+  inputDatasetName: "example_name",
+  inputDatasetSchema: "example_schema"
+});
